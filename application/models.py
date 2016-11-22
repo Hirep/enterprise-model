93911976ce5cb4dfe97a208b0f225c314fe638c1
Hirep <samoilenko.d.v@yandex.com>
2016-11-22 05:00:18 +0200

    'init'

--- application/models.py
+++ application/models.py
@@ -2,23 +2,23 @@ from application import db
 from datetime import datetime
 
 
-# class User(db.Model):
-#     __tablename__ = 'users'
+class User(db.Model):
+    __tablename__ = 'users'
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     name_first = db.Column(db.String(64))
-#     name_second = db.Column(db.String(64))
-#     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
-#     position_id = db.Column(db.Integer,  db.ForeignKey('departments.id'))
-#     email = db.Column(db.String(64), unique=True)
-#     tel = db.Column(db.String(15))
-#     birth_date = db.Column(db.Date(), default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    name_first = db.Column(db.String(64))
+    name_second = db.Column(db.String(64))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    position_id = db.Column(db.Integer,  db.ForeignKey('departments.id'))
+    email = db.Column(db.String(64), unique=True)
+    tel = db.Column(db.String(15))
+    birth_date = db.Column(db.Date(), default=datetime.utcnow)
 
-#     def __repr__(self):
-#         return "Name: {} {}\nEmail: {}\nTel: {}".format(self.name_first,
-#                             self.name_second,
-#                             self.email,
-#                             self.tel)
+    def __repr__(self):
+        return "Name: {} {}\nEmail: {}\nTel: {}".format(self.name_first,
+                            self.name_second,
+                            self.email,
+                            self.tel)
 
 class Department(db.Model):
     __tablename__ = 'departments'
--- dbutil.py
+++ dbutil.py
@@ -1,82 +1,82 @@
 from application import db
-from application.models import Position, Department
+from application.models import User, Position, Department
 import datetime
 
-db.create_all()
+# db.create_all()
 
-pos1 = Position(position_name='Private Coder', about='The lowest rank in enterprise hierarchy')
-db.session.add(pos1)
+# pos1 = Position(position_name='Private Coder', about='The lowest rank in enterprise hierarchy')
+# db.session.add(pos1)
 
-pos2 = Position(position_name='Head of department', about='This rank empowers you to kick your slaves')
-db.session.add(pos2)
+# pos2 = Position(position_name='Head of department', about='This rank empowers you to kick your slaves')
+# db.session.add(pos2)
 
-dpt1 = Department(department_name='Hydra Zero Research',
-                    head_id=1,
-                    parental_department_id=3,
-                    about='Some crazy things')
-db.session.add(dpt1)
+# dpt1 = Department(department_name='Hydra Zero Research',
+#                     head_id=1,
+#                     parental_department_id=3,
+#                     about='Some crazy things')
+# db.session.add(dpt1)
 
-dpt2 = Department(department_name='Election Department',
-                    head_id=2,
-                    parental_department_id=3,                    
-                    about='Make America Great Again!')
-db.session.add(dpt2)
+# dpt2 = Department(department_name='Election Department',
+#                     head_id=2,
+#                     parental_department_id=3,                    
+#                     about='Make America Great Again!')
+# db.session.add(dpt2)
 
-dpt3 = Department(department_name='Technical Department',
-                    head_id=5,
-                    about='-')
-db.session.add(dpt3)
+# dpt3 = Department(department_name='Technical Department',
+#                     head_id=5,
+#                     about='-')
+# db.session.add(dpt3)
 
 
-# usr1 = User(name_first='Rufus',
-#                 name_second='Sixsmith',
-#                 department_id=1,
-#                 position_id=2,
-#                 email='rufus.hydra.zero@q.uk',
-#                 tel='+380001234567',
-#                 birth_date=datetime.datetime.utcnow()
-#                                 )
-# db.session.add(usr1)
+usr1 = User(name_first='Rufus',
+                name_second='Sixsmith',
+                department_id=1,
+                position_id=2,
+                email='rufus.hydra.zero@q.uk',
+                tel='+380001234567',
+                birth_date=datetime.datetime.utcnow()
+                                )
+db.session.add(usr1)
 
-# usr2 = User(name_first='Donald',
-#                 name_second='Trump',
-#                 department_id=2,
-#                 position_id=2,
-#                 email='pussy.grabber@us.gov',
-#                 tel='+380001234569',
-#                 birth_date=datetime.datetime.utcnow()
-#                                 )
-# db.session.add(usr2)
+usr2 = User(name_first='Donald',
+                name_second='Trump',
+                department_id=2,
+                position_id=2,
+                email='pussy.grabber@us.gov',
+                tel='+380001234569',
+                birth_date=datetime.datetime.utcnow()
+                                )
+db.session.add(usr2)
 
-# usr3 = User(name_first='Brett',
-#                 name_second='Cannon',
-#                 department_id=1,
-#                 position_id=1,
-#                 email='tall.canadian@py.ca',
-#                 tel='+380001232469',
-#                 birth_date=datetime.datetime.utcnow()
-#                                 )
-# db.session.add(usr3)
+usr3 = User(name_first='Brett',
+                name_second='Cannon',
+                department_id=1,
+                position_id=1,
+                email='tall.canadian@py.ca',
+                tel='+380001232469',
+                birth_date=datetime.datetime.utcnow()
+                                )
+db.session.add(usr3)
 
-# usr4 = User(name_first='Lorne',
-#                 name_second='Malvo',
-#                 department_id=2,
-#                 position_id=1,
-#                 email='fargoslayer@us.com',
-#                 tel='+380001632469',
-#                 birth_date=datetime.datetime.utcnow()
-#                                 )
-# db.session.add(usr4)
+usr4 = User(name_first='Lorne',
+                name_second='Malvo',
+                department_id=2,
+                position_id=1,
+                email='fargoslayer@us.com',
+                tel='+380001632469',
+                birth_date=datetime.datetime.utcnow()
+                                )
+db.session.add(usr4)
 
-# usr5 = User(name_first='Elon',
-#                 name_second='Musk',
-#                 department_id=3,
-#                 position_id=2,
-#                 email='mars2020@us.com',
-#                 tel='+380011632469',
-#                 birth_date=datetime.datetime.utcnow()
-#                                 )
-# db.session.add(usr5)
+usr5 = User(name_first='Elon',
+                name_second='Musk',
+                department_id=3,
+                position_id=2,
+                email='mars2020@us.com',
+                tel='+380011632469',
+                birth_date=datetime.datetime.utcnow()
+                                )
+db.session.add(usr5)
 
 try:
     db.session.commit()
